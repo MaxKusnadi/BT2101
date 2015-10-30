e4a084f8e045b7d12cb3656ab175268199c40f43
MaxKusnadi <max.kusnadi@gmail.com>
2015-10-30 09:04:22 +0800

    O

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 // RADAR
+var radarData = {
+	"canon":{
+		data:[13.31, 38.59, 40.1, 14.37, 27.81, 28.86, 24.02, 33.04]
+	},
+	"nikon":{
+		data:[12.16, 33.27, 39.86, 13.49, 31.47, 20.12, 21.07, 29.43]
+	},
+	"sony":{
+		data:[9.78, 22.21, 33.63, 5.52, 32.79, 12.07, 15.79, 18.42]
+	},
+	"fuji":{
+		data:[7.74,	23.7, 37.17, 14.67, 35.84, 17.99, 14.7, 16.17]
+	},
+	"olympus":{
+		data:[11.13, 22.29,	36.14, 18.28, 33.82, 14.4, 16.15, 17.35]
+	},
+	"panasonic":{
+		data:[9.32,	25.25, 33.85, 18.18, 34.66, 12.8, 16.99, 18.54]
+	},
+	"pentax":{
+		data:[13.66, 30.52, 37.47, 14.83, 35.13, 26.27, 15.32, 26.58]
+	}
+}
 
 var radar = {
-		labels: ["Price", "Size", "Lens", "AAAAAAA", "BBBBBB"],
+		labels: ["Build Quality", "Battery Life", "Handling Ergonomics", "Size", "User-Friendliness", "Price", "Weight", "Auto-Focus"],
 		datasets: [
 			{
 				label: "Canon Dataset",
@@ -11,7 +34,7 @@ var radar = {
 				pointStrokeColor: "#fff",
 				pointHighlightFill: "#fff",
 				pointHighlightStroke: "rgba(220,220,220,1)",
-				data: [65,59,90,81,56]
+				data: radarData["canon"].data
 			}
 		]
 	};
@@ -21,56 +44,97 @@ var myRadar = new Chart(document.getElementById("chart1").getContext("2d")).Rada
 var nikonBut = document.getElementById('nikonRadar');
 
 function nikonR(){
-	console.log("HAHA")
-	myRadar.datasets[0].points[0].value = 30;
-	myRadar.datasets[0].points[1].value = 10;
-	myRadar.datasets[0].points[2].value = 70;
-	myRadar.datasets[0].points[3].value = 20;
-	myRadar.datasets[0].points[4].value = 80;
+	for (var i = 0; i < 8;i++){ 
+		myRadar.datasets[0].points[i].value = radarData["nikon"].data[i];
+	}
 	myRadar.update();
 };
 
 function canonR(){
-	console.log("HAHA")
-	myRadar.datasets[0].points[0].value = 65;
-	myRadar.datasets[0].points[1].value = 59;
-	myRadar.datasets[0].points[2].value = 90;
-	myRadar.datasets[0].points[3].value = 81;
-	myRadar.datasets[0].points[4].value = 56;
+	for (var i = 0; i < 8;i++){ 
+		myRadar.datasets[0].points[i].value = radarData["canon"].data[i];
+	}
+	myRadar.update();
+};
+
+function fujiR(){
+	for (var i = 0; i < 8;i++){ 
+		myRadar.datasets[0].points[i].value = radarData["fuji"].data[i];
+	}
+	myRadar.update();
+};
+
+function sonyR(){
+	for (var i = 0; i < 8;i++){ 
+		myRadar.datasets[0].points[i].value = radarData["sony"].data[i];
+	}
+	myRadar.update();
+};
+
+function olympusR(){
+	for (var i = 0; i < 8;i++){ 
+		myRadar.datasets[0].points[i].value = radarData["olympus"].data[i];
+	}
+	myRadar.update();
+};
+
+function panasonicR(){
+	for (var i = 0; i < 8;i++){ 
+		myRadar.datasets[0].points[i].value = radarData["panasonic"].data[i];
+	}
+	myRadar.update();
+};
+
+function pentaxR(){
+	for (var i = 0; i < 8;i++){ 
+		myRadar.datasets[0].points[i].value = radarData["pentax"].data[i];
+	}
 	myRadar.update();
 };
 
 // PIE
 var pieData = [
 				{
-					value: 300,
+					value: 40.1,
 					color:"#F7464A",
 					highlight: "#FF5A5E",
-					label: "Nikon"
-				},
-				{
-					value: 50,
-					color: "#46BFBD",
-					highlight: "#5AD3D1",
 					label: "Canon"
 				},
 				{
-					value: 100,
+					value: 18.3,
+					color: "#46BFBD",
+					highlight: "#5AD3D1",
+					label: "Nikon"
+				},
+				{
+					value: 18.7,
 					color: "#FDB45C",
 					highlight: "#FFC870",
 					label: "Sony"
 				},
 				{
-					value: 40,
+					value: 8.3,
 					color: "#949FB1",
 					highlight: "#A8B3C5",
-					label: "Theodore"
+					label: "Fuji"
 				},
 				{
-					value: 120,
+					value: 8.5,
 					color: "#4D5360",
 					highlight: "#616774",
-					label: "Chan Khan"
+					label: "Panasonic"
+				},
+				{
+					value: 4.2,
+					color: "#949FB1",
+					highlight: "#A8B3C5",
+					label: "Olympus"
+				},
+				{
+					value: 0.07,
+					color: "#4D5360",
+					highlight: "#616774",
+					label: "Sony"
 				}
 
 			];
